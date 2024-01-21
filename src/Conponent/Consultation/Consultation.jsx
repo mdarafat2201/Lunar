a4776f0409edeac211851efbba4162a73ce0ee84
mdarafat2201 <maccenarafat@gmail.com>
2024-01-21 20:39:18 +0000

    ConsultationPart

--- src/Conponent/Consultation/Consultation.jsx
+++ src/Conponent/Consultation/Consultation.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Consultation.css";
+import Button from "../CommonComponent/button";
 
 const Consultation = () => {
   return (
     <div className="ConsultationPart">
-      <div className="container">ugytf</div>
+      <div className="container">
+        <div className="ConsultationWrapper">
+          <div className="ConsultationWrapper__left">
+            <h1 className="WrapperLeft__titel">
+              Book A Free Digital Marketing Consultation
+            </h1>
+          </div>
+          <div className="ConsultationWrapper__right">
+            <input
+              type="text"
+              placeholder="Your Email"
+              className="WrapperRight__Email"
+            />
+            <Button btnstyle="meetingWrapper__btn">Book my free meeting</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
